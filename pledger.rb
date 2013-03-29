8341cec5c210d155883c9d6e952a534bef44336f
Nathaniel Jones <n@nthj.me>
2013-03-29 14:15:47 -0500

    Database initialization

--- pledger.rb
+++ pledger.rb
@@ -1,17 +1,32 @@
 require 'bundler'
 Bundler.require
 
+
+## DATABASE INITIALIZATION
+
 Sequel.connect(ENV['DATABASE_URL'])
+
+DB.create_table :pledges do
+  primary_key :id # user ID
+  String :email
+  Integer :amount_cents
+end
+
+
+## INITIAL SETUP
 
 enable :sessions
 
+
+## APP
+
 get '/' do
-  session[:user_id] ||= 'what'
+  session[:user_id] ||= DB[:pledges].insert()
 end
 
 post '/pledges' do
   DB[:pledges].where(id: session[:user_id]).update(
-    amount: params[:amount],
+    amount_cents: params[:amount_cents],
     email: params[:email]
   )
 
