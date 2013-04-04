7e73c1d5e1681c1f818300e1c58400f3400f5f07
Nathaniel Jones <n@nthj.me>
2013-04-04 17:00:09 -0500

    Who cares about the nodes

--- public/assets/pledger.js
+++ public/assets/pledger.js
@@ -379,16 +379,16 @@ var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; 
 */
 
 /* Prevent page from moving around on touch */
-node.addEventListener("touchmove", function(event){
-    // Only deal with one finger    
-    if(event.touches.length == 1){ 
-        // Get the information for finger #1
-        var touch = event.touches[0], 
-            // Find the style object for the node the drag started from
-            style = touch.target.style; 
-        // Position the element under the touch point
-        style.position = "absolute"; 
-        style.left = touch.pageX + "px";
-        style.top = touch.pageY + "px";
-    }
-}, false);
+// node.addEventListener("touchmove", function(event){
+//     // Only deal with one finger
+//     if(event.touches.length == 1){
+//         // Get the information for finger #1
+//         var touch = event.touches[0],
+//             // Find the style object for the node the drag started from
+//             style = touch.target.style;
+//         // Position the element under the touch point
+//         style.position = "absolute";
+//         style.left = touch.pageX + "px";
+//         style.top = touch.pageY + "px";
+//     }
+// }, false);
